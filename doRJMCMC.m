a88ab96bfd03f99c57371b6a054a631f3186f745
neuhoffd <daniel.neuhoff@googlemail.com>
2015-11-17 17:36:28 +0100

    progressbar

--- doRJMCMC.m
+++ doRJMCMC.m
@@ -9,10 +9,13 @@ function [states, accepted] = doRJMCMC(settings)
     states(1) = getInitialState(settings);
 
     accepted = 0;
+    
+    progressbar;
 
     %Iterate until settings.draws is reached
     for cntrDraws = 2:settings.draws   
         [state, draw] = RJMCMCStep(states(cntrDraws-1), settings);
+        progressbar(cntrDraws/settings.draws);
         if settings.saveProposals
             PROPOSALS_GLOBAL(cntrDraws) = draw;
         end;
@@ -20,9 +23,11 @@ function [states, accepted] = doRJMCMC(settings)
         accepted = accepted + draw.accepted;
         states(cntrDraws) = state; 
 
-        %Report every 10000th draw
-        if mod(cntrDraws,10000) == 0
-            disp(['Iteration ' num2str(cntrDraws) '; Acceptance rate ' num2str(accepted/cntrDraws) ]);
-        end;
+%       Report every 10000th draw - DEACTIVATED FOR PROGRESSBAR
+%         if mod(cntrDraws,10000) == 0
+%             disp(['Iteration ' num2str(cntrDraws) '; Acceptance rate ' num2str(accepted/cntrDraws) ]);
+%         end;
     end;
+    
+    progressbar(1);
 end
